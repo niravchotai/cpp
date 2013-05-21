8fb4f48ca40736e5d148f870f77298f1004c5c9f
Nirav Chotai <niravchotai.ce@gmail.com>
2013-05-21 22:24:40 +0530

    The Circle class (All source codes in one file)

---
+++ circleClass/circleClass.cpp
@@ -0,0 +1,67 @@
+/*
+ * circleClass.cpp
+ *
+ *  Created on: 14-Apr-2013
+ *      Author: nirav.chotai
+ */
+/* The Circle class (All source codes in one file) */
+#include <iostream>    // using IO functions
+#include <string>      // using string
+using namespace std;
+
+class Circle {
+private:
+   double radius;      // Data member (Variable)
+   string color;       // Data member (Variable)
+
+public:
+   // Constructor with default value for data members
+   Circle(double r = 1.0, string c = "red") {
+      radius = r;
+      color = c;
+   }
+
+   double getRadius() {  // Member function (Getter)
+      return radius;
+   }
+
+   string getColor() {   // Member function (Getter)
+      return color;
+   }
+
+   double getArea() {    // Member function
+      return radius*radius*3.1416;
+   }
+   // Setter for color
+   void setColor(string c) {
+      color = c;
+   }
+
+   // Setter for radius
+   void setRadius(double r) {
+      radius = r;
+   }
+};   // need to end the class declaration with a semi-colon
+
+// Test driver function
+int main() {
+   // Construct a Circle instance
+   Circle c1(1.2, "blue");
+   cout << "Radius=" << c1.getRadius() << " Area=" << c1.getArea()
+        << " Color=" << c1.getColor() << endl;
+
+   // Construct another Circle instance
+   Circle c2(3.4); // default color
+   cout << "Radius=" << c2.getRadius() << " Area=" << c2.getArea()
+        << " Color=" << c2.getColor() << endl;
+
+   // Construct a Circle instance using default no-arg constructor
+   Circle c3;      // default radius and color
+   cout << "Radius=" << c3.getRadius() << " Area=" << c3.getArea()
+        << " Color=" << c3.getColor() << endl;
+
+   return 0;
+}
+
+
+
