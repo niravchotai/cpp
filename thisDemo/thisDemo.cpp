c5a0adee8a5b36c48c28a79359235755e10e4f00
Nirav Chotai <niravchotai.ce@gmail.com>
2013-05-26 14:45:04 +0530

    This keyword demo

---
+++ thisDemo/thisDemo.cpp
@@ -0,0 +1,25 @@
+/*
+ * thisDemo.cpp
+ *
+ *  Created on: 14-Apr-2013
+ *      Author: nirav.chotai
+ */
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+class Circle {
+private:
+	double radius;
+public:
+	double getRadius() const {
+		return radius;
+	}
+
+	void setRadius(double radius) {
+		this->radius = radius;
+	}
+}
+
+
+
